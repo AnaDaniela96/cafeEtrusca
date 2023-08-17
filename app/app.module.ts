6c269f494a7cbbe3b678de72a7f1b641625678b4
desarrolladoraFront <daniela.andrade@cafeetrusca.com>
2023-08-17 12:01:20 -0600

    Genera el contenido de la vista home pero no esta completa, hace falta matizar detalles, y construir el div que contiene las últimas entradas del blog.

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { TerminosYCondicionesComponent } from './pages/terminos-y-condiciones/terminos-y-condiciones.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    MenuComponent
+    MenuComponent,
+    TerminosYCondicionesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
